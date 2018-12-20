65df14fcff0624135f98ed3576c4d678ca2e6817
lkafedzic1 <zivotinje>
2018-12-20 07:32:50 +0100

    first commit

---
+++ src/ba/unsa/etf/rpr/Drzava.java
@@ -0,0 +1,28 @@
+package ba.unsa.etf.rpr;
+
+public class Drzava {
+    private String naziv;
+    private Grad glavniGrad;
+
+    public Drzava(){}
+    public Drzava (String naziv, Grad glavniGrad) {
+        this.naziv = naziv;
+        this.glavniGrad = glavniGrad;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public Grad getGlavniGrad() {
+        return glavniGrad;
+    }
+
+    public void setGlavniGrad(Grad glavniGrad) {
+        this.glavniGrad = glavniGrad;
+    }
+}
